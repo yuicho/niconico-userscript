760c66db8d566c7ca46156f8cc7de983abef50f5
Yuicho <43964607+yuicho@users.noreply.github.com>
2021-06-29 21:02:42 +0900

    Create eraser_of_likes_counter.user.js

---
+++ eraser_of_likes_counter.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Eraser of likes counter
+// @namespace    https://yuicho.net
+// @version      0.1
+// @description  I hate counter of likes.
+// @author       Yuicho
+// @match        https://www.nicovideo.jp/*
+// @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    $('.count.like').hide();
+})();
